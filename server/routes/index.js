e68a0b2ff9becd6a10d3a8f60f41bd28472bf3ca
Ivan M <karlos123jun@hotmail.com>
2022-11-15 09:57:23 -0600

    Feat: Protected default path without access token

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,7 +1,10 @@
+const expressAccessToken = require("express-access-token");
 const express = require("express");
 const app = express();
 
-app.use("/default", require("./default"));
-app.use('/privileges',require('./privileges'))
+const firewall = require("../security/accesstoken");
+
+app.use("/default", expressAccessToken, firewall, require("./default"));
+app.use("/privileges", require("./privileges"));
 
 module.exports = app;
