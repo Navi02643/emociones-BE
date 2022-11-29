1097c28a1d9e494d04464df6fc71df2b6c0dd73e
Ivan M <karlos123jun@hotmail.com>
2022-11-28 18:47:56 -0600

    Fix: Added Env Variables

--- server/config/db.js
+++ server/config/db.js
@@ -1,15 +1,5 @@
-/* eslint-disable no-console */
 const mongoose = require("mongoose");
 require("dotenv").config();
-
-process.env.PORT = process.env.PORT || 5000;
-process.env.NODE_ENV = process.env.NODE_ENV || "dev";
-
-if (process.env.NODE_ENV === "dev") {
-  process.env.URLDB = "mongodb+srv://admin:admin@emotionsbe.cketmac.mongodb.net/emotionsBE";
-} else {
-  process.env.URLDB = "mongodb+srv://admin:admin@emotionsbe.cketmac.mongodb.net/emotionsBE";
-}
 
 process.middlewares = [];
 
