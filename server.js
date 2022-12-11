a25a779e752e93f3c44367a3d497c193c249714e
Miguel Angel Salas Flores <miguelangel.sf@outlook.com>
2022-12-11 14:46:00 -0600

    Swagger document

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use((req, res, next) => {
     "Access-Control-Allow-Headers",
     "Authorization, Origin, X-Requested-With, Content-Type, Accept",
   );
-  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE. FETCH");
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE, FETCH");
   res.header("Allow", "GET, POST, OPTIONS, PUT, DELETE, FETCH");
   next();
 });
