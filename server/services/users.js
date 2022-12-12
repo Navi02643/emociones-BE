bc566a0d025df058275319e286c80c19cc6cb4e0
Ivan M <karlos123jun@hotmail.com>
2022-12-12 11:09:14 -0600

    Feat: required field validation added

--- server/services/users.js
+++ server/services/users.js
@@ -7,9 +7,11 @@ async function generatePassword() {
   const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
   let password = '';
   const charactersLength = characters.length;
+
   for (let i = 0; i < 10; i += 1) {
     password += characters.charAt(Math.floor(Math.random() * charactersLength));
   }
+
   return password;
 }
 
@@ -21,11 +23,13 @@ async function sendEmail(user, password) {
       pass: process.env.PASSWORD,
     },
   });
+
   const mailOptions = {
     from: process.env.EMAIL,
     to: user.email,
     subject: 'Confirmacion de registro',
-    text: `Te damos la bienvenida ${user.fullName}, tus datos de acceso son:
+    text: `
+    Te damos la bienvenida ${user.fullName}, tus datos de acceso son:
     correo: ${user.email}
     contraseña: ${password}
     Se te recomienda cambiar tu contraseña una vez ingreses a la pagina`,
@@ -34,17 +38,39 @@ async function sendEmail(user, password) {
   return 0;
 }
 
+function checkUserData(user) {
+  try {
+    if (user.name === "") return { isValid: false, message: 'the name is required', data: [] };
+    if (user.middleName === "") return { isValid: false, message: 'the middleName is required', data: [] };
+    if (user.email === "") return { isValid: false, message: 'the email is required', data: [] };
+    if (user.phone === "") return { isValid: false, message: 'the phone is required', data: [] };
+    if (user.birthdate === "") return { isValid: false, message: 'the birthday is required', data: [] };
+    if (user.idRecord === "") return { isValid: false, message: 'the record is required', data: [] };
+    return true;
+  } catch (error) {
+    return { isValid: false, message: 'the name, middleName, email, phone and record is obligatory', data: [] };
+  }
+}
+
 async function generateUser(user) {
+  const check = checkUserData(user);
+
+  if (check.isValid === false) return check;
+
   const userData = user;
   const findEmail = await userDB.findEmail(user.email);
-  if (findEmail) return 'Email already exists';
+
+  if (findEmail) return { isValid: false, message: 'Email already exists', data: user.email };
+
   const password = await generatePassword();
   const passwordEncrypted = await bcrypt.hash(password, 10);
   userData.password = passwordEncrypted;
   const userSave = await userDB.saveUser(userData);
   const dataUserFilter = userDTO.filterUser(userSave);
+
   sendEmail(dataUserFilter, password);
-  return dataUserFilter;
+
+  return { isValid: true, message: 'User successfully created', data: dataUserFilter };
 }
 
 module.exports = { generateUser };
