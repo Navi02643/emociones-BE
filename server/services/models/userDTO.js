29c55f308403880caaad5e7224fbcb7450c634f9
Ivan M <karlos123jun@hotmail.com>
2022-12-16 10:10:53 -0600

    Fix: issue with record fixed

--- server/services/models/userDTO.js
+++ server/services/models/userDTO.js
@@ -4,8 +4,8 @@ function checkUserData(user) {
   try {
     const schema = Joi.object({
       name: Joi.string().pattern(/^[a-zA-Z ]+$/, { name: 'alpha' }),
-      middleName: Joi.string().pattern(/^[a-zA-Z]+$/, { name: 'alpha' }),
-      lastName: Joi.string().pattern(/^[a-zA-Z]+$/, { name: 'alpha' }),
+      middleName: Joi.string().pattern(/^[a-zA-Z ]+$/, { name: 'alpha' }),
+      lastName: Joi.string().pattern(/^[a-zA-Z ]+$/, { name: 'alpha' }),
       email: Joi.string().email(),
       phone: Joi.number(),
       birthdate: Joi.date(),
