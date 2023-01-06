1901ce8923ce5e1eb4b2055516988f38941892c3
ErickIsra01 <erickisrael620@gmail.com>
2023-01-06 09:46:03 -0600

    Express endpoint deleted

--- server/routes/index.js
+++ server/routes/index.js
@@ -9,7 +9,6 @@ app.use("/login", require("./login"));
 app.use("/user", expressAccessToken, firewall, require("./users"));
 app.use("/session", expressAccessToken, firewall, require("./session"));
 app.use("/appointments", expressAccessToken, firewall, require("./appointments"));
-app.use("/videoCall", expressAccessToken, firewall, require("./videoCall"));
 app.use("/notification", expressAccessToken, firewall, require('./notifications'));
 
 module.exports = app;
