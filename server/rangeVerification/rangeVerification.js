49e813f80a14bb3c15e1719bb48aedaeb88abecd
ErickIsra01 <erickisrael620@gmail.com>
2023-01-18 13:48:00 -0600

    Fixed crashing problem

--- server/rangeVerification/rangeVerification.js
+++ server/rangeVerification/rangeVerification.js
@@ -4,8 +4,9 @@ const userDB = require("../database/users");
 
 const therapistAndAdminRange = async (req, res, next) => {
   const { idUser } = await tokenDB.findToken(req.headers.authorization.split(" ")[1]);
-  const { range } = await userDB.findById(idUser);
-  if (range === RANGE.admin || range === RANGE.therapist) return next();
+  const userData = await userDB.findById(idUser);
+  if (!userData) return res.status(401).send("Unauthorized");
+  if (userData.range === RANGE.admin || userData.range === RANGE.therapist) return next();
   return (res.status(403).send({
     isValid: false,
     message: "Requires grant permission",
