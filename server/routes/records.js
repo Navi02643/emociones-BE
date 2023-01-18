5ca5fe6ff71f044d22d7f14dd88e5bb7d5f3697f
ErickIsra01 <erickisrael620@gmail.com>
2023-01-18 13:33:15 -0600

    Added getting multiple records endpoint

--- server/routes/records.js
+++ server/routes/records.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const rangeRequired = require("../rangeVerification/rangeVerification");
 const recordService = require("../services/records");
 
 const app = express();
@@ -29,6 +30,23 @@ app.delete('/', async (req, res) => {
     });
   } catch (error) {
     return res.status(500).send({
+      isValid: false,
+      message: 'something failed, try again later',
+      data: null,
+    });
+  }
+});
+
+app.get('/', rangeRequired, async (req, res) => {
+  try {
+    const data = await recordService.getRecords(req.headers.authorization.split(" ")[1], req.query);
+    return res.status(200).send({
+      isValid: data.isValid,
+      message: data.message,
+      data: data.data,
+    });
+  } catch (error) {
+    return res.status(200).send({
       isValid: false,
       message: 'something failed, try again later',
       data: null,
