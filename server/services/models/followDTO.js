3e43dd9a2f4084c0da198948dee9c861e6d5f39b
Ivan M <karlos123jun@hotmail.com>
2023-01-24 10:56:46 -0600

    Fix: A constant was created for the months

--- server/services/models/followDTO.js
+++ server/services/models/followDTO.js
@@ -1,11 +1,11 @@
+const MONTHS = require('../../utils/months.constant');
+
 function filterFollow(follow) {
   const year = (follow.date).getFullYear();
-  const months = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'];
-  let day = (follow.date).getDate();
+  let day = (follow.date).getDate() + 1;
   const month = (follow.date).getMonth();
-
   if (day <= 9) day = `0${day}`;
-  const date = `${day}-${months[month]}-${year}`;
+  const date = `${day}-${MONTHS[month]}-${year}`;
   return {
     id: follow.id,
     date,
